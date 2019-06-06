f97db0e1ecc25397a5c8a5ee2a4b67358d674da9
Lazjh <hes16005@byui.edu>
2019-06-06 10:02:51 -0600

    Add project files.

---
+++ ScriptureJournal/Migrations/20190604165658_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScriptureJournal.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JournalEntry",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EntryDate = table.Column<DateTime>(nullable: false),
+                    Cannon = table.Column<string>(nullable: true),
+                    Book = table.Column<string>(nullable: true),
+                    Chapter = table.Column<string>(nullable: true),
+                    Verse = table.Column<string>(nullable: true),
+                    Entry = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JournalEntry", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JournalEntry");
+        }
+    }
+}
---
+++ ScriptureJournal/Models/JournalEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScriptureJournal.Models
+{
+    public class JournalEntry
+    {
+        public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EntryDate { get; set; }
+        public string Cannon { get; set; }
+        public string Book { get; set; }
+        public string Chapter { get; set; }
+        public string Verse { get; set; }
+        public string Entry { get; set; }
+
+
+    }
+}
