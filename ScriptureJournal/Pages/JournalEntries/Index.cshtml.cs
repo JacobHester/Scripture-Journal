ff4f1461f7205171d4ffc0f1d5c07387b881996c
Lazjh <hes16005@byui.edu>
2019-06-08 16:33:20 -0600

    Finished app

--- ScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
+++ ScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
@@ -18,6 +18,7 @@ namespace ScriptureJournal.Pages.JournalEntries
             _context = context;
         }
 
+        // Searching Variables
         public IList<JournalEntry> JournalEntry { get;set; }
         [BindProperty(SupportsGet = true)]
         public string SearchBook { get; set; }
@@ -25,8 +26,42 @@ namespace ScriptureJournal.Pages.JournalEntries
         [BindProperty(SupportsGet = true)]
         public string SearchEntry { get; set; }
 
-        public async Task OnGetAsync()
+        // Sorting Variables
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
+
+            // Sorting Section
+            CurrentSort = sortOrder;
+            BookSort = string.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<JournalEntry> journalIQ = from s in _context.JournalEntry
+                                                 select s;
+
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    journalIQ = journalIQ.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    journalIQ = journalIQ.OrderBy(s => s.EntryDate);
+                    break;
+                case "date_desc":
+                    journalIQ = journalIQ.OrderByDescending(s => s.EntryDate);
+                    break;
+                default:
+                    journalIQ = journalIQ.OrderBy(s => s.Cannon);
+                    break;
+            }
+
+
+
+            // Searching Section
             var journalEntries = from m in _context.JournalEntry
                                  select m;
 
@@ -40,6 +75,7 @@ namespace ScriptureJournal.Pages.JournalEntries
                 journalEntries = journalEntries.Where(s => s.Entry.Contains(SearchEntry));
             }
 
+            JournalEntry = await journalIQ.AsNoTracking().ToListAsync();
             JournalEntry = await journalEntries.ToListAsync();
         }
     }
